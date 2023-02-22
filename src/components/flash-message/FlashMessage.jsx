57f2a2f2791aafc37454b3bbfead847d98c2f289
Eyub-Abdi <ayubabdiy@gmail.com>
2023-02-21 23:24:54 -0800

    Finish flash message component design

--- src/components/flash-message/FlashMessage.jsx
+++ src/components/flash-message/FlashMessage.jsx
@@ -4,9 +4,9 @@ import './flashMessage.scss'
 function FlashMessage() {
   return (
     <div className="floating-alerts">
-      <div className="alert floating-alert">New user has been created successfully</div>
+      <div className="alert-warning floating-alert">New user has been created successfully</div>
     </div>
   )
 }
-// Test git hub push token
+
 export default FlashMessage
